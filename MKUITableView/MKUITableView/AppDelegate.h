a5103a9eaf14991abfb61adcb39155ff941a89f6
mangesh_kshirsagar <mangesh@vaultize.com>
2016-03-03 12:29:07 +0530

    Adding basic schema for the project.

---
+++ MKUITableView/MKUITableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKUITableView
+//
+//  Created by Mangesh on 03/03/16.
+//  Copyright © 2016 mangesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKUITableView/MKUITableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKUITableView
+//
+//  Created by Mangesh on 03/03/16.
+//  Copyright © 2016 mangesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
